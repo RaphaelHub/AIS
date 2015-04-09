b5263d4be19f786972a589c5c31b7da076646b75
RaphaelHub <raphael-gruber@hotmail.com>
2015-04-09 18:58:38 +0200

    blub

--- Robot/src/com/example/robot/Read.java
+++ Robot/src/com/example/robot/Read.java
@@ -15,24 +15,34 @@ public class Read implements Runnable {
 		// while(true){
 		String string = MainActivity
 				.comReadWrite(new byte[] { 'q', '\r', '\n' });
+		string = string.trim();
 		String[] arr = string.split(" ");
 		int[] sensor = new int[arr.length];
-
-		/* cast form hexa to dezimal */
+		
+		/* cast form hexa to dezimal 2 -> first two indices are ' '*/
 		for (int i = 0; i < arr.length; i++) {
+			System.out.println(sensor[i]);
 			/*Substring because of 0x...*/
+			try {
 			sensor[i] = Integer.parseInt(arr[i].substring(2), 16);
+			} catch (Exception e ) {
+				
+			}
 		}
-
-		int links = sensor[2];
-		int rechts = sensor[4];
-		int mitte = sensor[3];
-
-		if (links != 0 || rechts != 0 || mitte != 0) {
-			/* stop vehicle */
-			MainActivity.comReadWrite(new byte[] { 's', '\r', '\n' });
-			controller.setStatus(true);
-		}
+System.out.println(sensor[2]);
+		
+//	int links = sensor[2];
+//	int rechts = sensor[4];
+//	int mitte = sensor[3];
+//	
+//	System.out.println(links + " " + mitte +" " + rechts);
+//
+//		if (links != 0 || rechts != 0 || mitte != 0) {
+//			/* stop vehicle */
+//			//MainActivity.comReadWrite(new byte[] { 's', '\r', '\n' });
+//			//controller.setStatus(true);
+//			System.out.println("stop");
+//		}
 
 		/*
 		 * try { Thread.sleep(5000); } catch (InterruptedException e) { // TODO
