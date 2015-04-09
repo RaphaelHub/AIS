753f0adbd38949ba38630ccb4c752ed5c581121a
RaphaelHub <raphael-gruber@hotmail.com>
2015-04-09 17:06:29 +0200

    improve

--- Robot/src/com/example/robot/Read.java
+++ Robot/src/com/example/robot/Read.java
@@ -13,35 +13,34 @@ public class Read implements Runnable {
 	@Override
 	public void run() {
 		// while(true){
-		String string = MainActivity.comReadWrite(new byte[] { 'q', '\r', '\n' });
+		String string = MainActivity
+				.comReadWrite(new byte[] { 'q', '\r', '\n' });
 		String[] arr = string.split(" ");
-		int [] sensor = new int [arr.length];
-		
+		int[] sensor = new int[arr.length];
+
+		/* cast form hexa to dezimal */
 		for (int i = 0; i < arr.length; i++) {
-			sensor[i] = Integer.parseInt(arr[i],16);
+			/*Substring because of 0x...*/
+			sensor[i] = Integer.parseInt(arr[i].substring(2), 16);
 		}
-		
+
 		int links = sensor[2];
 		int rechts = sensor[4];
 		int mitte = sensor[3];
-		
+
 		if (links != 0 || rechts != 0 || mitte != 0) {
+			/* stop vehicle */
 			MainActivity.comReadWrite(new byte[] { 's', '\r', '\n' });
 			controller.setStatus(true);
 		}
-		
 
 		/*
-		try {
-			Thread.sleep(5000);	
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		MainActivity.comReadWrite(new byte[] { 's', '\r', '\n' });
-
-		controller.setStatus(true);
-		// }*/
+		 * try { Thread.sleep(5000); } catch (InterruptedException e) { // TODO
+		 * Auto-generated catch block e.printStackTrace(); }
+		 * MainActivity.comReadWrite(new byte[] { 's', '\r', '\n' });
+		 * 
+		 * controller.setStatus(true); // }
+		 */
 
 	}
 
