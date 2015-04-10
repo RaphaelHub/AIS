5c26ee577ebbb7f17b2f1c7396c5e63e2ce3a96d
RaphaelHub <raphael-gruber@hotmail.com>
2015-04-10 17:10:20 +0200

    neu

--- Robot/src/com/example/robot/MainActivity.java
+++ Robot/src/com/example/robot/MainActivity.java
@@ -41,6 +41,7 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void onClick(View arg0) {
+				// robotSetVelocity(b,b);
 				comWrite(new byte[] { 'w', '\r', '\n' });
 			}
 		});
