7bdecae9b7350839953dc85ecc6a115c86fb4bf8
philipphaeusle <philipp.haeusle@hotmail.com>
2015-04-15 12:28:34 +0200

    Stopwatch

--- Robot/src/com/example/robot/Stopwatch.java
+++ Robot/src/com/example/robot/Stopwatch.java
@@ -2,15 +2,18 @@ package com.example.robot;
 
 public class Stopwatch { 
 
-    private final long start;
+    private long start;
 
     public Stopwatch() {
-        start = System.currentTimeMillis();
     } 
 
     public double elapsedTime() {
         long now = System.currentTimeMillis();
         return (now - start) / 1000.0;
+    }
+    
+    public void start(){
+    	start = System.currentTimeMillis();
     }
 
 } 
